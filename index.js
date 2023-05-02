6a9377840431da7d413930396e4f6641e29e5420
Louis Klein <louisklein71@gmail.com>
2023-05-02 15:38:20 -0700

    test

--- index.js
+++ index.js
@@ -10,7 +10,39 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+ 
 
-const titleCased = () => {
-  return tutorials
+
+//const titleCased = tutorials.map((questions) => {
+//  for (let j=0; j<tutorials.length; j++){
+//    let string = tutorials[j];
+//   let capitalize = string.split("")
+//    for (let i=0; i<capitalize.length;i++){
+//      capitalize[i]=capitalize[i].charAt(0).toUpperCase() + capitalize[i].slice(1);
+//       }
+//    return questions
+//  }})
+  
+
+
+//return tutorials.charAt(0).toUpperCase() + tutorials.slice(1);
+
+
+
+console.log("start");
+const titleCased = () => { 
+  const result = tutorials.map((tutorial) => { 
+  var capitalizeString = tutorial.split(" ");
+  for(let i=0; i<capitalizeString.length; i++){
+    capitalizeString[i] = capitalizeString[i].charAt(0).toUpperCase() + capitalizeString[i].slice(1);
+    console.log(capitalizeString)
+    
+    }
+  return capitalizeString.join(" ");
+
+ })    
+ console.log(result)
+
+ return result
 }
+console.log(titleCased());
